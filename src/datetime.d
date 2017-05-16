c106e39a5cf67e32df884e0112d92fb6c3444c07
Mrokkk <mrokk@protonmail.com>
2017-05-16 22:00:14 +0200

    Show calendar on right click on the datetime block

--- src/datetime.d
+++ src/datetime.d
@@ -1,14 +1,18 @@
 module datetime;
 
 import std.format : format;
+import std.process : executeShell;
 import std.datetime : msecs, Clock;
 
 import event : event;
 import formatter : formatter;
 import config : PORT, TEMPLATE, powerline_look;
 
-string datetime_handler(event) {
+string datetime_handler(event ev) {
     auto current_time = Clock.currTime();
+    if (ev == event.right_click) {
+        executeShell("notify-send \"`cal`\"");
+    }
     if (powerline_look) {
         return "<span color=\"#00ccff\"></span><span background=\"#00ccff\"><span color=\"#0d0d0d\"><b>DATETIME</b></span></span> %s, %d %s %d, %02d:%02d:%02d".format(
             current_time.dayOfWeek, current_time.day, current_time.month,
