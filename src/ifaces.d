389589651cef473261710ea0db394b284a40fdaa
Mrokkk <mrokk@protonmail.com>
2017-05-19 22:28:46 +0200

    Refactoring ifaces.d

--- src/ifaces.d
+++ src/ifaces.d
@@ -27,22 +27,27 @@ extern (C) void freeifaddrs(ifaddrs *ifa);
 
 class ifaces : blocklet {
 
-    void call(block_layout f) {
+    private void add_interface(block_layout b, ifaddrs *iface) {
+        if (iface.ifa_addr.sa_family != AddressFamily.INET) {
+            return;
+        }
+        auto iface_name = iface.ifa_name.to!string;
+        if (iface_name != "lo") {
+            b.add_value("%s: %d.%d.%d.%d".format(
+                iface.ifa_name.to!string,
+                iface.ifa_addr.sa_data[0],
+                iface.ifa_addr.sa_data[1],
+                iface.ifa_addr.sa_data[2],
+                iface.ifa_addr.sa_data[3]));
+        }
+    }
+
+    void call(block_layout b) {
         ifaddrs* ifaces = null;
         getifaddrs(&ifaces);
         ifaddrs* temp = ifaces;
         while (ifaces.ifa_next != cast(ifaddrs*)0) {
-            if (ifaces.ifa_addr.sa_family is AddressFamily.INET) {
-                auto iface_name = ifaces.ifa_name.to!string;
-                if (iface_name != "lo") {
-                    f.add_value("%s: %d.%d.%d.%d".format(
-                        ifaces.ifa_name.to!string,
-                        ifaces.ifa_addr.sa_data[0],
-                        ifaces.ifa_addr.sa_data[1],
-                        ifaces.ifa_addr.sa_data[2],
-                        ifaces.ifa_addr.sa_data[3]));
-                }
-            }
+            add_interface(b, ifaces);
             ifaces = ifaces.ifa_next;
         }
         freeifaddrs(temp);
