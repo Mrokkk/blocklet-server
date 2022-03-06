5680c53b9942041bd8905a4ef39339b56c7bd49e
Mrokkk <mrokk@protonmail.com>
2022-03-06 15:56:42 +0100

    Update deps and fix battery
    Updated vibe-d to 0.9.4 and changed battery blocklet to change color
    depending on the level and send notification if level is critically low

--- src/battery.d
+++ src/battery.d
@@ -4,17 +4,40 @@ import std.conv : to;
 import std.string: strip;
 import std.format : format;
 import std.algorithm : map;
+import std.process : executeShell;
 import std.file : readText, dirEntries, SpanMode;
 
-import formatter : block_layout;
+import formatter : block_layout, colors;
 import blocklet : blocklet, event;
 
 class battery : blocklet {
 
     void call(block_layout f) {
-        f.add_value("%(%d%| %)".format(dirEntries(
+        int mean = 0;
+        int numberOfValues = 0;
+
+        foreach (val; dirEntries(
             "/sys/class/power_supply/", "BAT{0,1}", SpanMode.depth, false)
-                .map!(a => (a.name ~ "/capacity").readText().strip().to!int)));
+                .map!(a => (a.name ~ "/capacity").readText().strip().to!int))
+        {
+            numberOfValues++;
+            mean += val;
+
+            colors color = colors.normal;
+
+            if (val < 15) {
+                color = colors.red;
+            }
+            else if (val < 30) {
+                color = colors.yellow;
+            }
+            f.add_value("%d".format(val), color);
+        }
+
+        if (mean / numberOfValues <= 5)
+        {
+            executeShell("notify-send '!!! Low battery !!!' 'Battery level is low' --icon=dialog-information -u critical -c im.error");
+        }
     }
 
     void handle_event(event) {
